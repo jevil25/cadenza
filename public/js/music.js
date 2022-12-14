7c64f87cc23655a4e7364e67a2d6bb9bd59db83a
AaronNazareth <4nm20cs005@nmamit.in>
2022-12-14 14:05:34 +0530

    added search bar

--- public/js/music.js
+++ public/js/music.js
@@ -124,19 +124,3 @@ function myFunction() {
   popup.classList.toggle("show");
 }
 
-document.querySelector('#search-input').addEventListener('input',filterList)
-
-function filterList(){
-  const searchInput=document.querySelector("#search-input");
-  const filter=searchInput.value.tolowerCase();
-  const listItems=document.querySelectorAll('.list-group-item');
-
-  listItems.forEach((item)=>{
-    let text=item.textContent;
-    if(text.toLowerCase().includes(filter.tolowerCase())){
-        item.style.display='';
-    }else{
-      item.style.display='none';
-    }
-  });
-}
